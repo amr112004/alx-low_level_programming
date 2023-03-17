1ca8b7e8d5bdcebb767c431489ab7c851a77e9b1
amr <amr112004>
2023-03-17 10:01:18 -0700

    My first cmmmit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- * 
- * Descriotion: A C program that prints with printf function.
- * 
- * Return: Always 0 (Success) 
+ * main - Entry point
+ *
+ * Description: A C progam that prints with printf function.
+ *
+ * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
