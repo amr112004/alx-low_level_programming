6855851b3abe39e455b38c607f0459b224a51407
amr <amr112004>
2023-03-23 08:07:02 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
-#include "main.h"
-
-/**
- * print_alphabet - utilizes on the _putchar function to print
- *	   the alphabet a - z
-*/
-
-void print_alphabet(void)
-{
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar("\n");
-}
+ALX 2023, [23/03/2023 03:09 م]
+#include <unistd.h> 
+#include "main.h" 
+ 
+/** 
+ * _putchar - writes the character c to stdout 
+ * @c: The character to print 
+ * 
+ * Return: On success 1. 
+ *         On error, -1 is retu/
