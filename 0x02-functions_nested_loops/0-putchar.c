fef767a821161b941745cc7f90ab997f5680c3af
amr <amr112004>
2023-03-23 04:46:02 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -Entry point 
+ *
+ * Description; prints -putchar using putchar prototype 
+ *
+ * Return : Alwayes 0 (success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+	for (ch = 0: ch < 8: ch++)
+		_putchar(str[ch]);
+	-putchar("\n");
+
+	return (0);
+}	
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unsisted.h>
+#include "main.h"
+
+/**
+ *  _putchar - writes the character c to stdout
+ *  @c: The character to print 
+ *
+ *  Return: On success 1.
+ *          On error, -1 is returned, and errno is set appropiatel
+*/
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
