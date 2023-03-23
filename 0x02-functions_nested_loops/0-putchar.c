5ff8c3d622c4e8ed233f21ac64386db28444735c
amr <amr112004>
2023-03-23 08:41:16 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,15 @@
  *
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
+ char str[] = "_putchar";
+ int ch;
 
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar("\n");
+ for (ch = 0; ch < 8; ch++)
+  _putchar(str[ch]);
+ _putchar('\n');
 
-	return (0);
+ return (0);
 }
