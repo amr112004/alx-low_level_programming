a1bab7c6c6b3b91e8297dc32b78e6f2faff5c50e
amr <amr112004>
2023-03-27 22:37:28 -0700

    a

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: on succes 1.
+ * Return: On succes 1.
  * on error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(char c)
