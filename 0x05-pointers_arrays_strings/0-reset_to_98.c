c3432b7e275fe5fc086ab6810666970354b0adb9
amr <amr112004>
2023-03-27 22:29:30 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_90 - takes a pointer to am int parameter
+ *			and updates the value is points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
