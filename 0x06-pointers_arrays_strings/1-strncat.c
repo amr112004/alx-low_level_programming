b95a6ff9cf35c77516759e4e1f6596f056f3c829
amr <amr112004>
2023-03-31 08:25:48 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,9 +5,9 @@
  *
  * @dest: pointer to destination input
  * @src: pointer to source input
- * @n: most number of byte from 8src
+ * @n: most number of byte from @src
  *
- * Return: 3dest
+ * Return: @dest
 */
 
 char *_strncat(char *dest, char *src, int n)
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function taht copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: byets of @src
+ *
+ * Return: @dest
+*/
+
