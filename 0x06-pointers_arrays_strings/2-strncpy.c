ad6d7cccf381b0b2b44edf5cc0ac130529f7659e
amr <amr112004>
2023-03-31 20:17:12 -0700

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,28 @@
 #include "main.h"
-
 /**
- * _strncpy - A function taht copies a string.
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * @dest: pointer to destination input buffer
- * @src: pointer to source input buffer
- * @n: byets of @src
- *
- * Return: @dest
-*/
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
 
