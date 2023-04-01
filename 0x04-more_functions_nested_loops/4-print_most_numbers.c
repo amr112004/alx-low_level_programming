4279292474ad9b290631f5e00a93f374763b2b6f
amr <amr112004>
2023-03-31 21:40:16 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,25 +1,19 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_most_numbers - print 0 - 9 apart
- *		from 2 and 4 and you can
- *		only use putchar twice
- * Return: Always 0 (Success)
-*/
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
+ */
 
 void print_most_numbers(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num <= 9; num++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (num == 2 || num == 4)
-			continue;
-		putchar(num + 48)
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
-
-
-
-
