71bd6db871939b218ae971045b7ff824d5f4ff29
amr <amr112004>
2023-03-31 20:15:50 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,31 @@
 #include "main.h"
-
 /**
- * _strnact - a function that concatenates tow strings.
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * @dest: pointer to destination input
- * @src: pointer to source input
- * @n: most number of byte from @src
- *
- * Return: @dest
-*/
-
+ * Return: dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int i;
+	int j;
 
-	c = 0;
-
-	/*find size of dest array*/
-	while (dest[c])
-		c++;
-
-	/**
-	 * src does not need to be null terminated
-	 * if it contains n or more bytes
-	*/
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	/*null torminate dest*/
-	dest[c + i] = '\0';
-	
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
+
